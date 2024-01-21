b9e1c91e27bfeae0e8cb77b2b9091be2ecf10055
Ahban08 <mary870818@yahoo.com.tw>
2024-01-21 21:47:42 +0800

    add 1.cpp Solution_2

--- Easy/1.TwoSum.cpp
+++ Easy/1.TwoSum.cpp
@@ -17,3 +17,27 @@ public:
     }
 };
 //Time Complexity: O(N^2)
+//Space Complexity: O(1)
+
+class Solution_2 {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res(2);
+        std::map<int, int> numsmap; //key紀錄數值，value紀錄位置
+        int temp;
+        for (int i = 0; i < nums.size(); i++) {
+            temp = target-nums[i];
+            if (numsmap.find(temp)== numsmap.end()) {
+                // Element not found in the map
+                numsmap.insert(std::pair<int, int>(nums[i], i));
+            }else{
+                res[0] = numsmap[temp];
+                res[1] = i;
+                return res;
+            }
+        }
+        return res;
+    }
+};
+//Time Complexity: O(N)
+//Space Complexity: O(N)
