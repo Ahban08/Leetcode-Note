c7fac622d16635cef2effd023f79df0c6fd0a515
Ahban08 <mary870818@yahoo.com.tw>
2024-01-22 12:00:24 +0800

    Update Follow Up of 242.

--- Easy/242.Valid Anagram.cpp
+++ Easy/242.Valid Anagram.cpp
@@ -25,3 +25,33 @@ public:
 };
 //Time Complexity: O(N)
 //Space Complexity: O(1)
+
+//Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+//如果使用unicode，即無法使用array的方式進行比較，可轉而使用hashmap
+class Solution_2 {
+public:
+    bool isAnagram(string s, string t) {
+        std::map<char,int> counter;
+        char letterS, letterT;
+        bool res = true;
+        if (s.length() != t.length()){
+            res = false;
+        }else{
+            for (int i = 0; i < s.length(); i++){
+                letterS = s[i];
+                counter[letterS]++;
+                letterT = t[i];
+                counter[letterT]--;
+            }
+            for (auto pair : counter){
+                if (pair.second != 0){
+                    res =false;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
+//Time Complexity: O(N)
+//Space Complexity: O(1)
