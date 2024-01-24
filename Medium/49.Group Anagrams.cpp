7216643a8ead62d5c2859b6843f2d40e5c479a4c
Ahban08 <mary870818@yahoo.com.tw>
2024-01-25 00:36:50 +0800

    update 49.cpp- using 2-dim array to record letter distribution

--- Medium/49.Group Anagrams.cpp
+++ Medium/49.Group Anagrams.cpp
@@ -22,3 +22,36 @@ public:
 //M (the number of strings) and N (the average length of strings)
 //Time Complexity: O(M*NlogN)
 //Space Complexity: O(M)
+
+//將sort()換成二維陣列紀錄字串的字母分布
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> res;
+        std::map<string, int> groupMap;
+        int set= 0;
+        for (int i = 0; i < strs.size(); i++){
+            int letterDistribution[26] ={};
+            for (int j = 0; j < strs[i].length(); j++){
+                int letterAscii = int(strs[i].at(j));
+                letterDistribution[letterAscii-int('a')]++;
+            }
+            string letterCount = "";
+            for (int k = 0; k < 26; k++){
+                letterCount.append(to_string(letterDistribution[k]));
+                letterCount.append(",");
+            }
+            if(groupMap.find(letterCount)!= groupMap.end()){
+                res[groupMap[letterCount]].push_back(strs[i]);
+            }else{
+                groupMap[letterCount] = res.size();
+                res.resize(res.size() + 1);
+                res[groupMap[letterCount]].push_back(strs[i]);
+            }
+        }
+        return res;
+    }
+};
+//M (the number of strings) and N (the average length of strings)
+//Time Complexity: O(M*N)
+//Space Complexity: O(M)
