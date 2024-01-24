4e54ae58f24b63103cf5609318837ff8badf9e41
Ahban08 <mary870818@yahoo.com.tw>
2024-01-24 22:46:44 +0800

    add 49.cpp

---
+++ Medium/49.Group Anagrams.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> res;
+        std::map<string, int> groupMap;
+        int set= 0;
+        for (int i = 0; i < strs.size(); i++){
+            string sortedStr = strs[i];
+            std::sort (sortedStr.begin(), sortedStr.end());
+            if(groupMap.find(sortedStr)!= groupMap.end()){
+                res[groupMap[sortedStr]].push_back(strs[i]);
+            }else{
+                groupMap[sortedStr] = res.size();
+                //cout<<"size:"<<res.size();
+                res.resize(res.size() + 1);
+                res[groupMap[sortedStr]].push_back(strs[i]);
+            }
+        }
+        return res;
+    }
+};
