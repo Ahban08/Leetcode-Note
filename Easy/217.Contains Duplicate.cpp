6248072aa6d351d16550d0f5a721c9f3b7777fc5
Ahban08 <mary870818@yahoo.com.tw>
2024-01-29 15:07:50 +0800

    Add Solution_2 of 217.cpp

--- Easy/217.Contains Duplicate.cpp
+++ Easy/217.Contains Duplicate.cpp
@@ -18,3 +18,24 @@ public:
 };
 //Time Complexity: O(N)
 //Space Complexity: O(N)
+
+class Solution_2 {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        bool res = false;
+        set<int> count; //用set儲存陣列掃過的值
+
+        for(int i= 0; i < nums.size(); i++){
+            if( count.find(nums[i]) != count.end()){
+                // cout<<"count[nums[i]]:nums[i]"<<count[nums[i]]<<":"<<nums[i];
+                res = true;
+                return res;
+            }else{
+                count.insert(nums[i]);
+            }
+        }
+        return res;
+    }
+};
+//Time Complexity: O(N)
+//Space Complexity: O(N)
