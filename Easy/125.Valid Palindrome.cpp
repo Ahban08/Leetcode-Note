ec925ba70336b656b8e8bf98cc7a4fbed03f5aaf
Ahban08 <mary870818@yahoo.com.tw>
2024-01-31 15:34:48 +0800

    Add 125.cpp

---
+++ Easy/125.Valid Palindrome.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        bool res = true;
+        string str;
+        size_t front = 0, back;
+        
+        for(size_t i = 0;  i < s.length(); i++){
+            char c = tolower(s.at(i));
+            if(isalnum(c)){
+                str.push_back(c);
+            }
+        }
+        if(str.length()>1){
+            back = str.length()-1;
+        }else{
+            return res;
+        }
+
+        for(int i = 0;  front < back; i++){
+            if( str.at(front) != str.at(back)){
+                res = false;
+                break;
+            }
+            if(front == str.length() || back == 0){
+                break;
+            }
+            else{
+                front++;
+                back--;
+            }
+        }
+        return res;
+    }
+};
+//Time Complexity: O(N)
+//Space Complexity: O(N)
+
+int main() {
+    Solution sol;
+    string input = " ";
+    cout << "Is palindrome: " << sol.isPalindrome(input) << endl;
+    return 0;
+}
