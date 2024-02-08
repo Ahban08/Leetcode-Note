257c617f5c90bd16c16aba4fb886da637e76061e
Ahban08 <mary870818@yahoo.com.tw>
2024-02-08 15:35:21 +0800

    Add 33.cpp

---
+++ Medium/33. Search in Rotated Sorted Array.cpp
@@ -0,0 +1,75 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int res = -1, left, right;
+
+        left = 0;
+        right =nums.size()-1;
+        while(left <= right){
+            int mid = (left + right)/2;
+            if(nums[mid] == target){
+                res = mid;
+                break;
+            }
+            
+
+            if(nums[mid] < target && target <= nums[right]){
+                left = mid + 1;
+            }else if(nums[left] <= target && target < nums[mid]){
+                right = mid - 1;
+            }else{
+                if(nums[left] < nums[right]){
+                    break;
+                }else if( nums[mid] < nums[right]){ //target在左亂序
+                    right = mid - 1;
+                }else if( nums[left] < nums[mid]){ //target在右亂序
+                    left = mid + 1;
+                }else{
+                    left++;
+                }
+                
+            }
+            // cout<<"left"<<left<<"right"<<right<<"\n";
+        }
+        return res;
+    }
+};
+//Time Complexity: O(logN)
+//Space Complexity: O(1)
+
+class Solution_1 {
+public:
+    int search(vector<int>& nums, int target) {
+        int res = -1, left, right;
+
+        left = 0;
+        right =nums.size()-1;
+        while(left <= right){
+            int mid = (left + right)/2;
+            if(nums[mid] == target){
+                res = mid;
+                break;
+            }else if(left == right){
+                break;
+            }
+            
+            if( nums[mid] <= nums[right]){ //右有序
+                if(nums[mid] < target && target <= nums[right]){
+                    left = mid + 1;
+                }else{
+                    right = mid - 1;
+                }
+            }else if( nums[left] <= nums[mid]){ //左有序
+                if(nums[left] <= target && target < nums[mid]){
+                    right = mid - 1;
+                }else{
+                    left = mid + 1;
+                }
+            }
+
+        }
+        return res;
+    }
+};
+//Time Complexity: O(logN)
+//Space Complexity: O(1)
