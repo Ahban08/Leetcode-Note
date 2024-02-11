aa8dd7c1d3225408fd9223cc150a0b9518c1d2bd
Ahban08 <mary870818@yahoo.com.tw>
2024-02-11 22:54:09 +0800

    Add 141.cpp Follow up

--- Easy/141. Linked List Cycle.cpp
+++ Easy/141. Linked List Cycle.cpp
@@ -17,3 +17,30 @@ public:
         return res;
     }
 };
+//Time Complexity: O(N)
+//Space Complexity: O(N)
+
+//Follow up: Can you solve it using O(1) (i.e. constant) memory?
+//Floyd's Cycle Finding Algorithm
+//利用Two Pointer一快一慢前進，若input中包含cycle則必定在有限時間內相遇
+class Solution_2 {
+public:
+    bool hasCycle(ListNode *head) {
+        bool res = false;
+        ListNode *faster = head;
+        if(head==nullptr){
+            return res;
+        }
+        while( faster->next!= nullptr && faster->next->next!= nullptr){
+            head = head->next;
+            faster = faster->next->next;
+            if(head == faster){
+                res = true;
+                break;
+            }
+        }
+        return res;
+    }
+};
+//Time Complexity: O(N)
+//Space Complexity: O(1)
