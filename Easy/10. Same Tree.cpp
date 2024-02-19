af9a1630765751c6d8bb40c22207d0d1661919cc
Ahban08 <mary870818@yahoo.com.tw>
2024-02-19 10:10:11 +0800

    Add 10.cpp

---
+++ Easy/10. Same Tree.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        bool res = true;
+        if(res != false){
+            if(p == nullptr || q == nullptr){
+                if(p != nullptr || q != nullptr){
+                    res= false;
+                }
+                return res;
+            }
+            if(p->val == q->val){
+                if(!isSameTree(p->left, q->left) || !isSameTree(p->right, q->right)){
+                    res = false;
+                }
+            }else{
+                res = false;
+            }
+        }
+        return res;
+    }
+};
+//Time Complexity: O(N)
+//Space Complexity: O(1)
