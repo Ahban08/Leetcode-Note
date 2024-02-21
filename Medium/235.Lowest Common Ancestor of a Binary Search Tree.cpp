a535a6f21d8c309718d639cbf341d2715c8743ea
Ahban08 <mary870818@yahoo.com.tw>
2024-02-21 20:59:46 +0800

    Update 235.cpp Interactive method

--- Medium/235.Lowest Common Ancestor of a Binary Search Tree.cpp
+++ Medium/235.Lowest Common Ancestor of a Binary Search Tree.cpp
@@ -21,3 +21,47 @@ public:
 };
 //Time Complexity: O(logN)
 //Space Complexity: O(logN)
+
+class Solution_1 {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        TreeNode* res = root;
+
+        int value = root->val;
+        if( p->val < value && q->val < value){
+            res = lowestCommonAncestor(root->left, p, q);
+        }else if( p->val > value && q->val > value){
+            res = lowestCommonAncestor(root->right, p, q);
+        }
+        return res;
+    }
+};
+//Time Complexity: O(logN)
+//Space Complexity: O(logN)
+
+//Interactive
+class Solution_2 {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        TreeNode* res;
+        stack<TreeNode*> nodeStack;
+
+        nodeStack.push(root);
+        while(!nodeStack.empty()){
+            TreeNode *node = nodeStack.top();
+            nodeStack.pop();
+            int value = node->val;
+            if( p->val < value && q->val < value){
+                nodeStack.push(node->left);
+            }else if( p->val > value && q->val > value){
+                nodeStack.push(node->right);
+            }else{
+                res = node;
+                break;
+            }
+        }
+        return res;
+    }
+};
+//Time Complexity: O(logN)
+//Space Complexity: O(1)
